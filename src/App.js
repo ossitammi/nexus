492c67cbd1d81d2f432280a42d3dafbe13c05634
Hyperion <ossi.tammi@gmail.com>
2020-12-16 22:15:11 +0200

    Added simple hex grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  column: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+  block: {
+    height: '50px',
+    width: '50px',
+    border: '1px solid white',
+    backgroundColor: 'lightblue'
+  }
+});
+
+const rows = new Array(9).fill(0);
+const columns = Array(9).fill(0);
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div> 
-      Nexus has awaken
+    <div>
+      {rows.map((row, i) => {
+        return (
+          <div key={i} className={classes.row}>
+            {columns.map((column, j) => {
+              return (
+                <div className={classes.block} key={`index-${i}-${j}`}></div>
+              )
+            })}
+          </div>
+        );
+      })}
     </div>
   )
 }
